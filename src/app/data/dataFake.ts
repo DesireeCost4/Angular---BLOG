4d2d6af15259e4f9b1fe0961346f491a77dcacfe
DesireeCost4 <desireec.dev@gmail.com>
2024-02-19 17:28:40 -0300

    upando

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+
+    { 
+        "id":"1",
+        "title": "EQUILIBRIO",
+        "description": "Redução do Estresse e da Ansiedade: O exercício físico libera endorfinas, neurotransmissores responsáveis pela sensação de bem-estar. Quando nos exercitamos, o corpo também libera serotonina e dopamina, que ajudam a reduzir a ansiedade e melhoram o humor. Além disso, o exercício proporciona uma pausa nas preocupações mentais, permitindo que a mente se acalme e relaxe.",
+        "picCover":"https://conteudo.imguol.com.br/c/entretenimento/87/2023/05/05/saude-mental-tecnologia-computador-1683303063686_v2_1920x1280.jpg"
+    },
+
+    { 
+        "id": "2",
+        "title": "Migrar de carreira para a área de tecnologia pode ser um desafio emocionante, mas também gratificante.",
+        "description": "Bem, poderia simplesmente copiar e colar este texto, como muitos têm feito, e de fato, talvez devesse. No entanto, levando em consideração minha dedicação em aprender e valorizando o conhecimento, assim como o tempo dedicado pelos envolvidos na organização do conteúdo, gostaria de deixar algumas palavras sobre os conhecimentos adquiridos. Deixo claro que, enquanto escrevo este texto, estou no meio do curso de Angular, no módulo de prática, onde construímos esta página web que aqui se apresenta. Simplesmente saltei da utilização de HTML, CSS e JavaScript puros para a utilização do framework Angular. Admito que tem sido uma jornada complexa, mas que tem se desmistificado ao longo dos conteúdos que o Felipe, junto à DIO, estão oferecendo. Já a introdução, que trouxe os conceitos de componentização e reutilização dos componentes, foi simplesmente incrível, pois, como iniciante, nem sequer sabia qual a função dessa ferramenta incrível. Em seguida, além da componentização, aprendi passos importantes sobre etapas que precedem o próprio código, como a idealização da estrutura da página a ser construída, divisão de etapas de codificação, organização do projeto, bem como pôr em prática a base dos conhecimentos que já possuía. Chego a este momento com muita alegria, pois sei que estou em um caminho positivo de aprendizado, onde posso realmente transformar ideias em projetos reais, valorizando todo o meu conhecimento teórico e aplicando-o com empenho durante a prática em projetos. Fico feliz que esta seja apenas uma etapa da jornada, pois significa que ainda existe evolução. Vou em frente!",
+        "picCover": "https://tm.ibxk.com.br/2022/07/29/29110751726107.jpg"
+    },
+
+    { 
+        "id":"3",
+        "title": "Chegou com a capacidade de dar voz e traduzir anseios e sentimentos de gerações inteiras. ",
+        "description": "Esse dico me ajudou a enxergar luz durante a escuridão, espero que possa ajudar ou trazer ideias positivas.",
+        "picCover": "https://i.scdn.co/image/ab67616d0000b2735472481c1e1fe75b6c296145"
+    },
+
+    { 
+        "id":"4",
+        "title": "Para quem vai sua torcida?",
+        "description": "Vamos jogar um league of legends? Nick: Apepi",
+        "picCover": "https://cdn.folhape.com.br/upload/dn_arquivo/2023/12/cblol.jpg"
+    }
+]
